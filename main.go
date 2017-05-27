1cd219b548e7512228dd91433e9e6eeb8b3f781f
Jaska <pusanteemu@gmail.com>
2017-05-27 17:43:47 +0300

    Poistettut kommentoituja.

--- main.go
+++ main.go
@@ -1,25 +1,16 @@
 package main
 
-import (
-	"github.com/koodinikkarit/wompatti/keijo"
-	KeijoService "github.com/koodinikkarit/wompatti/keijo_service"
-
-	"golang.org/x/net/context"
-)
+import "github.com/koodinikkarit/wompatti/wompatti"
 
 func main() {
-	// context := wompatti.CreateContext(
-	// 	"jaska",
-	// 	"asdf321",
-	// 	"localhost",
-	// 	"3306",
-	// 	"wompatti",
-	// 	"7856",
-	// )
+	context := wompatti.CreateContext(
+		"jaska",
+		"asdf321",
+		"localhost",
+		"3306",
+		"wompatti",
+		"5052",
+	)
 
-	// context.Start()
-
-	client := keijo.CreateKeijoClient("10.70.0.6", "12345")
-	//client.TurnOn(context.Background(), &KeijoService.TurnOnRequest{Address: 0})
-	client.ChangeSource(context.Background(), &KeijoService.ChangeSourceRequest{Source: KeijoService.Devices_RECORDING1, Destination: KeijoService.Devices_BROADCAST, SourceNumber: 3})
+	context.Start()
 }
